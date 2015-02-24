2e525d68d331d8197cc9bed864ff14c6dffb0388
mirrorecho <info@randallwest.com>
2015-02-23 22:46:54 -0600

    misc processing

--- processing/scribbles/build-tmp/source/scribbles.java
+++ processing/scribbles/build-tmp/source/scribbles.java
@@ -3,8 +3,6 @@ import processing.data.*;
 import processing.event.*; 
 import processing.opengl.*; 
 
-import java.util.*; 
-
 import java.util.HashMap; 
 import java.util.ArrayList; 
 import java.io.File; 
@@ -16,19 +14,145 @@ import java.io.IOException;
 
 public class scribbles extends PApplet {
 
+// import java.util.*;
+// Random gen = new Random();
 
-Random gen = new Random();
-
-public void setup() {
-  size(1280, 720);
-  background(60,40,44);
+// void setup() {
+//   size(1280, 720);
+//   background(60,40,44);
   
  
- //for (int x=0; x<numWalkers; x++) {
- //   walkers[x] = new Walker();
- //}
+//  //for (int x=0; x<numWalkers; x++) {
+//  //   walkers[x] = new Walker();
+//  //}
   
+// }
+
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+
+// SEE http://ejohn.org/apps/processing.js/examples/topics/scribbleplotter.html
+// All Examples Written by Casey Reas and Ben Fry
+// unless otherwise stated.
+// some scribble style constants that control 
+// how the scribble plotting works
+int SCRIBBLE = 0;
+int HATCHING = 1;
+
+public void setup(){
+  size(200, 200);
+  background(0);
+
+  // create arrays to hold x, y coords
+  float[]x = new float[4];
+  float[]y  = new float[4];
+  // create a convenient 2-dimensional 
+  // array to hold x, y arrays
+  float[][]xy = {x, y};
+
+  // record points
+  // x positions     
+  xy[0][0] = 25;
+  xy[0][1] = 175;
+  xy[0][2] = 175;
+  xy[0][3] = 25;
+
+  // y positions
+  xy[1][0] = 25;
+  xy[1][1] = 25;
+  xy[1][2] = 175;
+  xy[1][3] = 175;
+
+  // call plotting function
+  makeRect(xy);
 }
+
+public void makeRect(float[][]pts){
+  stroke(255);
+  smooth();
+  
+  // scribble variables, that get passed as arguments to the scribble function
+  int steps = 100;
+  float scribVal = 3.0f;
+  for (int i=0; i< pts[0].length; i++){
+    //plots vertices
+    strokeWeight(5);
+    point(pts[0][i], pts[1][i]);
+
+    // call scribble function
+    strokeWeight(.5f);
+    if (i>0){ 
+      scribble(pts[0][i], pts[1][i], pts[0][i-1], pts[1][i-1], steps, scribVal, SCRIBBLE);
+    } 
+    if (i== pts[0].length-1){
+    // show some hatching between last 2 points
+      scribble(pts[0][i], pts[1][i], pts[0][0], pts[1][0], steps, scribVal*2, HATCHING);
+    }
+  }
+}
+
+/* 
+scribble function plots lines between end points, 
+determined by steps and scribVal arguments.
+2 styles are available: SCRIBBLE and HATCHING, which
+are interestingly only dependent on parentheses
+placement in the line() function calls.
+*/
+public void scribble(float x1, float y1, float x2, float y2, int steps, float scribVal, int style){
+
+  float xStep = (x2-x1)/steps;
+  float yStep = (y2-y1)/steps;
+  for (int i = 0; i<steps; i++){
+    if(style == SCRIBBLE){
+      if (i<steps-1){
+        line(x1, y1, x1+=xStep+random(-scribVal, scribVal), y1+=yStep+random(-scribVal, scribVal));
+      } 
+      else {
+        // extra line needed to attach line back to point- not necessary in HATCHING style
+        line(x1, y1, x2, y2);
+      }
+    }
+    else if (style == HATCHING){
+       line(x1, y1, (x1+=xStep)+random(-scribVal, scribVal), (y1+=yStep)+random(-scribVal, scribVal));
+    }
+  }
+}
+
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+
+// SEE: http://www.openprocessing.org/sketch/17008
+//Scribble by Evan White
+// int x[];
+// int y[];
+// int pos=5;
+// int ellmove= 0;
+// void setup(){
+//   background(255);
+//   colorMode(HSB);
+//   size(800,600);
+//   smooth();
+//   noStroke();
+//   x= new int[pos];
+//   y=new int[pos];
+// }
+// void draw(){
+// for(int i=0; i<pos;i++){
+// fill(255);
+// stroke(0);
+// line(x[ellmove],y[ellmove],x[i],y[i]);
+// }}
+// void mouseMoved(){
+//   x[ellmove]=mouseX;
+//   y[ellmove]=mouseY;
+//   ellmove++;
+// if(ellmove>=pos){
+//   ellmove=0;
+// }}
+// void keyPressed(){
+//   if(key==32){
+//     background(255);
+//   }}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "scribbles" };
     if (passedArgs != null) {
